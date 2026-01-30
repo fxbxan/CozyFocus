256a769a130597d63e517b1dbc43317798a5214e
Fabian <fabjan1027@gmail.com>
2026-01-30 18:52:12 +0100

    env configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,7 +9,7 @@ export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
